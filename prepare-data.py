cc6478b940afacaaabbb70b99206a73f6fb08848
elsehow <yes@cosmopol.is>
2019-12-30 12:22:47 -0800

    Refactor prepare-data.py

--- prepare-data.py
+++ prepare-data.py
@@ -2,6 +2,13 @@ import glob
 import pickle
 from music21 import converter, instrument, note, chord
 
+def get_elements (midi):
+    try: # file has instrument parts
+        s2 = instrument.partitionByInstrument(midi)
+        return s2.parts[0].recurse()
+    except: # file has notes in a flat structure
+        return midi.flat.notes
+
 if __name__ == '__main__':
     """
     Save two pickle files: data/notes and data/durations.
@@ -10,24 +17,16 @@ if __name__ == '__main__':
     notes = []
     durations = []
 
+    def append_duration (el):
+        dur = float(el.duration.quarterLength)
+        durations.append(dur)
+
     for file in glob.glob("midi_songs/*.mid"):
-        midi = converter.parse(file)
-
         print("Parsing %s" % file)
+        midi = converter.parse(file)
+        elements = get_elements(midi)
 
-        notes_to_parse = None
-
-        try: # file has instrument parts
-            s2 = instrument.partitionByInstrument(midi)
-            notes_to_parse = s2.parts[0].recurse()
-        except: # file has notes in a flat structure
-            notes_to_parse = midi.flat.notes
-
-        def append_duration (el):
-            dur = float(el.duration.quarterLength)
-            durations.append(dur)
-
-        for element in notes_to_parse:
+        for element in elements:
             if isinstance(element, note.Note):
                 notes.append(str(element.pitch))
                 append_duration(element)
@@ -36,10 +35,10 @@ if __name__ == '__main__':
                 notes.append(chrd)
                 append_duration(element)
 
-    with open('data/notes', 'wb') as filepath:
-        pickle.dump(notes, filepath)
-    print('wrote data/notes')
+    # with open('data/notes', 'wb') as filepath:
+    #     pickle.dump(notes, filepath)
+    # print('wrote data/notes')
 
-    with open('data/durations', 'wb') as filepath:
-        pickle.dump(durations, filepath)
-    print('wrote data/durations')
+    # with open('data/durations', 'wb') as filepath:
+    #     pickle.dump(durations, filepath)
+    # print('wrote data/durations')
